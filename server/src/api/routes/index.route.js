84746fccad2849fd8d87ec2627d26f03fa0e17f2
Lillya <kidkaito1412.1@gmail.com>
2023-07-20 19:51:01 +0700

    fix relative path images

--- server/src/api/routes/index.route.js
+++ server/src/api/routes/index.route.js
@@ -1,23 +1,23 @@
-const userRoute = require('./user.route');
-const authRoute = require('./auth.route');
-const commentRoute = require('./comment.route');
-const postRoute = require('./post.route');
-const departmentRoute = require('./department.route');
-const documentRoute = require('./document.route');
-const subjectRoute = require('./subject.route');
-const topicRoute = require('./topic.route');
-const userSubjectRoute = require('./userSubject.route');
+const userRoute = require("./user.route");
+const authRoute = require("./auth.route");
+const commentRoute = require("./comment.route");
+const postRoute = require("./post.route");
+const departmentRoute = require("./department.route");
+const documentRoute = require("./document.route");
+const subjectRoute = require("./subject.route");
+const topicRoute = require("./topic.route");
+const userSubjectRoute = require("./userSubject.route");
 
 function route(app) {
-   app.use('/api/user', userRoute);
-   app.use('/api/auth', authRoute);
-   app.use('/api/comment', commentRoute);
-   app.use('/api/post', postRoute);
-   app.use('/api/department', departmentRoute);
-   app.use('/api/document', documentRoute);
-   app.use('/api/subject', subjectRoute);
-   app.use('/api/topic', topicRoute);
-   app.use('/api/user_subject', userSubjectRoute);
+  app.use("/api/user", userRoute);
+  app.use("/api/auth", authRoute);
+  app.use("/api/comment", commentRoute);
+  app.use("/api/post", postRoute);
+  app.use("/api/department", departmentRoute);
+  app.use("/api/document", documentRoute);
+  app.use("/api/subject", subjectRoute);
+  app.use("/api/topic", topicRoute);
+  app.use("/api/user_subject", userSubjectRoute);
 }
 
 module.exports = route;
